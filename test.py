7504596cf563a2c1aa26ec66234a52b651443ac9
mr.mohtarami <90931397+memal@users.noreply.github.com>
2023-06-03 17:27:38 +0330

    Add files via upload

---
+++ test.py
@@ -0,0 +1,15 @@
+# Import libraries
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import r2_score, mean_squared_error
+import yfinance as yf
+import datetime as dt
+import matplotlib.pyplot as plt
+import mplfinance as mplf
+import math
+
+
+
+
+print(1.1e9)
